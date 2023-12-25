2e7e610880dbb4e0039279cef634033a231ad174
unknown <abi.kh03@gmail.com>
2023-12-25 12:37:45 +1100

    hard coded div containers added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
  
 const App = () => {
   return (
-    <div>Hello world</div>
+    <div className="categories-container">
+      <div className="category-container">
+        {/*img*/}
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>SHOP NOW</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/*img*/}
+        <div className="category-body-container">
+          <h2>JACKETS</h2>
+          <p>SHOP NOW</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/*img*/}
+        <div className="category-body-container">
+          <h2>SNEAKERS</h2>
+          <p>SHOP NOW</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/*img*/}
+        <div className="category-body-container">
+          <h2>WOMENS</h2>
+          <p>SHOP NOW</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/*img*/}
+        <div className="category-body-container">
+          <h2>MENS</h2>
+          <p>SHOP NOW</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
