506ea879b5d5bdd878564d6c89ce5ce611e7b434
unknown <abi.kh03@gmail.com>
2023-12-25 12:46:04 +1100

    Making categories more dynamic

--- src/App.js
+++ src/App.js
@@ -1,44 +1,43 @@
  
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 1,
+      title: 'Jackets',
+    },
+    {
+      id: 1,
+      title: 'Sneakers',
+    },
+    {
+      id: 1,
+      title: 'Womens',
+    },
+    {
+      id: 1,
+      title: 'Mens',
+    }
+  ]
+
   return (
     <div className="categories-container">
-      <div className="category-container">
+      {categories.map(({title}) => (
+        <div className="category-container">
         {/*img*/}
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>SHOP NOW</p>
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>SHOP NOW</p>
+          </div>
         </div>
-      </div>
-      <div className="category-container">
-        {/*img*/}
-        <div className="category-body-container">
-          <h2>JACKETS</h2>
-          <p>SHOP NOW</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/*img*/}
-        <div className="category-body-container">
-          <h2>SNEAKERS</h2>
-          <p>SHOP NOW</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/*img*/}
-        <div className="category-body-container">
-          <h2>WOMENS</h2>
-          <p>SHOP NOW</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/*img*/}
-        <div className="category-body-container">
-          <h2>MENS</h2>
-          <p>SHOP NOW</p>
-        </div>
-      </div>
+
+      ))}
     </div>
   );
-}
+};
 
 export default App;
