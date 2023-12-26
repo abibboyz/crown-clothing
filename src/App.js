e9f42a735834d252a8d62e4abaea29e919576467
unknown <abi.kh03@gmail.com>
2023-12-26 21:58:39 +1100

    some cleanups and adding font

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ const App = () => {
   const categories = [
     {
       "id": 1,
-      "title": "hats",
+      "title": "Hats",
       "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
     },
     {
       "id": 2,
-      "title": "jackets",
+      "title": "Jackets",
       "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
     },
     {
       "id": 3,
-      "title": "sneakers",
+      "title": "Sneakers",
       "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
     },
     {
@@ -25,7 +25,7 @@ const App = () => {
     },
     {
       "id": 5,
-      "title": "mens",
+      "title": "Mens",
       "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
     }
   ]
